f7ff60fc9bd791a847fc0896df76b84c7bafbb2a
LeonChibanda <leonchibanda@icloud.com>
2025-03-24 07:52:58 +0200

    com-scre

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -4,33 +4,41 @@ using DocumentFormat.OpenXml.Spreadsheet;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    private static void Main()
     {
-        Comments _com = new Comments(); ;
-        Video _video1 = new Video();
-        _video1._title = "Star Trek: First Contact";
-        _video1._author = "Paramount Pictures";
-        _video1._length = 111;
+        Comments _com = new Comments();
+        Video _video1 = new Video
+        {
+            _title = "Star Trek: First Contact",
+            _author = "Paramount Pictures",
+            _length = 111
+        };
 
-        Video _video2 = new Video();
-        _video2._title = "Naruto: The Last Movie";
-        _video2._author = "Studio Pierrot";
-        _video2._length = 112;
+        Video _video2 = new Video
+        {
+            _title = "Naruto: The Last Movie",
+            _author = "Studio Pierrot",
+            _length = 112
+        };
 
-        Video _video3 = new Video();
-        _video3._title = "The Matrix";
-        _video3._author = "Warner Bros.";
-        _video3._length = 136;
+        Video _video3 = new Video
+        {
+            _title = "The Matrix",
+            _author = "Warner Bros.",
+            _length = 136
+        };
 
-        Video _video4 = new Video();
-        _video4._title = "Avengers: Endgame";
-        _video4._author = "Marvel Studios";
-        _video4._length = 181;
+        Video _video4 = new Video
+        {
+            _title = "Avengers: Endgame",
+            _author = "Marvel Studios",
+            _length = 181
+        };
 
         bool menuLoop = true;
         while (menuLoop)
         {
-            Console.WriteLine($"In what video you want to comment? \n1.- {_video1._title} \n2.- {_video2._title} \n3.- {_video3._title} \n4.- {_video4._title} \n5. - Quit");
+            Console.WriteLine($"In what video you want to comment? \n1.- {_video1._title} \n2.- {_video2._title} \n3.- {_video3._title} \n4.- {_video4._title} \n5.- Quit");
             int number = int.Parse(Console.ReadLine());
             if (number != 4)
             {
