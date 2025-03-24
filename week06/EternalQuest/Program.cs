2d68570ffe967694bc3b563d8189620572fb14b4
LeonChibanda <leonchibanda@icloud.com>
2025-03-24 23:29:44 +0200

    Et - Quest..Poly

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,9 +1,14 @@
+// Showed creativity by adding a function called GetRank(). 
+// This looks at the scores and determines what rank they are based on that scores, and outputs it to the console.
+// It is like grades where there is an A, B, C, D, and F. Ranks...except it is based on the score.
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        GoalManager goalManager = new GoalManager();
+
+        goalManager.Start();
     }
 }
