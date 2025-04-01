3e8bec810e5b0d8fe3c1363bb5f45f6a5e5bdf49
LeonChibanda <leonchibanda@icloud.com>
2025-04-01 18:54:45 +0200

    Exer Trac

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,33 @@
+public abstract class Activity
+{
+    private string _date;
+    protected float _length;
+
+    public Activity(string date, float length)
+    {
+        _date = date;
+        _length = length;
+    }
+    
+    public virtual double CalculateDistance()
+    {
+        return 0;
+    }
+    public virtual double CalculateSpeed()
+    {
+        return 0;
+    }
+    public virtual double CalculatePace()
+    {
+        return 0;
+    }
+    public string GetSummary()
+    {
+        var activityType = GetType().Name;
+        double distance = CalculateDistance();
+        double speed = CalculateSpeed();
+        double pace = CalculatePace();
+
+        return $"{_date} {activityType} ({_length} min) - Distance {Math.Round(distance, 1)} miles, Speed {Math.Round(speed, 1)} mph, Pace {Math.Round(pace, 1)} min per mile";
+    }
+}
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -2,8 +2,20 @@ using System;
 
 class Program
 {
+    //Leon Chibanda
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        Running running = new("29 March 2025", 30, 3);
+        Swimming swimming = new("31 March 2025", 60, 100);
+        Cycling  cycling = new("1 April 2025", 90, 13);
+        
+
+        List<Activity> activities = [running, swimming, cycling];
+
+        foreach(Activity activity in activities) 
+        {
+           string summary = activity.GetSummary();
+           Console.WriteLine(summary);
+        }
     }
 }
